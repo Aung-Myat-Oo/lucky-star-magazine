bdfc869d9525aa1c3e23549fc8e68eaa9ce42c99
aung-myat-oo <aungmyatoo2025@gmail.com>
2023-11-02 18:54:58 +0630

    push

--- lucky star project using bootstrap/new.js
+++ lucky star project using bootstrap/new.js
@@ -10,3 +10,33 @@ bar.addEventListener("click", () => {
     bar.classList.remove("fa-xmark");
   }
 });
+
+
+const clock = document.getElementById("clock");
+function updateTime() {
+  const time = new Date();
+  const hours = time.getHours();
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+  let timeSet = "";
+  timeSet += `${hours > 12 ? hours - 12 : hours}:${
+    minutes == 0 ? "" : minutes
+  }:${seconds}${hours > 12 ? " pm" : " am"}`;
+  clock.innerHTML = timeSet;
+}
+setInterval(updateTime, 1000);
+
+const moonIcon = document.querySelector(".fa-moon");
+moonIcon.addEventListener("click", () => {
+  if (moonIcon.classList.contains("fa-sun")) {
+    moonIcon.classList.remove("fa-sun");
+    moonIcon.classList.add("fa-moon");
+    document.body.style.backgroundColor = "white";
+    document.body.classList.toggle("dark-theme");
+  } else {
+    document.body.style.backgroundColor = "black";
+    moonIcon.classList.remove("fa-moon");
+    moonIcon.classList.add("fa-sun");
+    document.body.classList.toggle("dark-theme");
+  }
+});
--- lucky star project using bootstrap/script.js
+++ lucky star project using bootstrap/script.js
@@ -1,6 +1,6 @@
 const bar = document.querySelector(".fa-bars");
 const toggleNav = document.querySelector(".toggle-nav");
-const secText=document.querySelector(".sec-text")
+const secText = document.querySelector(".sec-text");
 bar.addEventListener("click", () => {
   toggleNav.classList.toggle("open");
   if (toggleNav.classList.contains("open")) {
@@ -11,18 +11,31 @@ bar.addEventListener("click", () => {
     bar.classList.remove("fa-xmark");
   }
 });
-
-
-const change=()=>{
+const change = () => {
   setTimeout(() => {
-    secText.textContent="Web Design"
+    secText.textContent = "Web Design";
   }, 0);
   setTimeout(() => {
-    secText.textContent="Printing"
+    secText.textContent = "Printing";
   }, 3500);
   setTimeout(() => {
-    secText.textContent="Magazine"
+    secText.textContent = "Magazine";
   }, 7000);
-}
-change()
-setInterval(change,10500)
+};
+change();
+setInterval(change, 10500);
+
+const moonIcon = document.querySelector(".fa-moon");
+moonIcon.addEventListener("click", () => {
+  if (moonIcon.classList.contains("fa-sun")) {
+    moonIcon.classList.remove("fa-sun");
+    moonIcon.classList.add("fa-moon");
+    document.body.style.backgroundColor = "white";
+    document.body.classList.toggle("dark-theme")
+  } else {
+    document.body.style.backgroundColor = "black";
+    moonIcon.classList.remove("fa-moon");
+    moonIcon.classList.add("fa-sun");
+    document.body.classList.toggle("dark-theme");
+  }
+});
